4fc05ceb0d4b80921676bfe9a166d2379e1a77bc
rulerhuang <rulerhuang@163.com>
2019-03-15 21:27:06 +0800

    来自vscode的提交

--- call_pylint.py
+++ call_pylint.py
@@ -1,17 +1,16 @@
-#encoding=utf-8
+# encoding=utf-8
 
-import commands
+
 from pylint import epylint
 
 if __name__ == '__main__':
-	(pylint_stdout,pylint_stderr) = epylint.py_run('just_for_test.py',return_std=True)
-	#(pylint_stdout,pylint_stderr) = epylint.py_run('test.py',return_std=True)
-	print pylint_stdout.read()
-	print pylint_stderr.read()
-	#print epylint.py_run('test.py')
-	#print epylint.py_run('just_for_test.py')
+    (pylint_stdout, pylint_stderr) = epylint.py_run('just_for_test.py', return_std=True)
+    # (pylint_stdout,pylint_stderr) = epylint.py_run('test.py',return_std=True)
+    print pylint_stdout.read()
+    print pylint_stderr.read()
+    # print epylint.py_run('test.py')
+    # print epylint.py_run('just_for_test.py')
 
-	#status,output = commands.getstatusoutput('pylint test.py')
-	#print status
-	#print output
-
+    # status,output = commands.getstatusoutput('pylint test.py')
+    # print status
+    # print output
